ec90328b3d972e9b1e8ed48474ba5e5639bd1ff8
Georgi Vachev <wachhew96@gmail.com>
2026-07-31 17:11:04 +0300

    fix: load assets properly, add debugger

--- img/index.js
+++ img/index.js
@@ -1,7 +1,7 @@
-import background from './background.jpg'
-import * as img from './*.png';
+import background from 'url:./background.jpg'
+import * as img from 'url:./*.png';
 
 export const urls = Object.freeze([
-    {alias: 'background', src: background},
-    ...Object.entries(img).map(([alias, src]) => ({alias, src}))
+    { alias: 'background', src: background },
+    ...Object.entries(img).map(([alias, src]) => ({ alias, src }))
 ]);
